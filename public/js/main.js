bb4cc9cbdd0b4d6ad3cad98552aed0deb47e9ec0
Andre Bitencourt <bitencourt.andre@hotmail.com>
2019-12-19 10:32:49 -0300

    [ CURSO 90% ]

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,7 @@ $(function(){
     atualizaTamanhoFrase();
     inicializaContadores();
     inicializaCronometro();
+    inicializaMarcadores();
     $("#botao-reiniciar").click(reiniciaJogo);
 });
 
@@ -38,11 +39,52 @@ function inicializaCronometro(){
             tempoRestante--;
             $("#tempo-digitacao").text(tempoRestante);
             if(tempoRestante < 1){
-                campo.attr("disabled",true);
                 clearInterval(cronometroID);
+                finalizaJogo();
             }
         },1000);
     });
+
+}
+
+
+function finalizaJogo(){
+    campo.attr("disabled",true);                
+    campo.toggleClass("campo-desativado");
+    inserePlacar();
+}
+
+function inicializaMarcadores(){
+
+    var frase = $(".frase").text();
+    campo.on("input", function(){
+        var digitado = campo.val();
+        var comparavel = frase.substr(0,digitado.length);
+
+        if(digitado == comparavel){
+            campo.addClass("borda-verde");
+            campo.removeClass("borda-vermelha");
+        }else{
+            campo.addClass("borda-vermelha");
+            campo.removeClass("borda-verde");
+        }
+
+    });
+
+}
+
+function inserePlacar(){
+
+    var tabela = $(".placar").find("tbody");
+    var usuario = "André";
+    var numPalavras = $("#contador-palavras").text();
+
+    var linha = "<tr>"+
+                    "<td>"+ usuario + "</td>"+
+                    "<td>"+ numPalavras + "</td>"+
+                "</tr>";
+
+    corpoTabela.prepend(linha);
 
 }
 
@@ -54,5 +96,8 @@ function reiniciaJogo(){
     $("#contador-caracteres").text("0");
     $("#tempo-digitacao").text(tempoInicial);
     inicializaCronometro();
+    campo.toggleClass("campo-desativado");
+    campo.removeClass("borda-verde");
+    campo.removeClass("borda-vermelha");
 
 }
